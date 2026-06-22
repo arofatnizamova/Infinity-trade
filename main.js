8a4bd1a361e1f0b3e3fcd05c2dc566f8c13e6285
Your Name <you@example.com>
2026-06-22 12:35:00 +0500

    feat: add 6 pages

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $('.slick-slider').each(function() {
+$(document).ready(function () {
+    $('.slick-slider').each(function () {
         let slider = $(this);
         let options = {
             prevArrow: slider.parent().find('.slider-prev'),
@@ -18,26 +18,57 @@ $(document).ready(function() {
                 dots: false,
                 autoplay: true,
                 responsive: [{
-                        breakpoint: 1200,
-                        settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 992,
-                        settings: {
-                            slidesToShow: 2,
-                            slidesToScroll: 1,
-                        }
-                    },
-                    {
-                        breakpoint: 576,
-                        settings: {
-                            slidesToShow: 1,
-                            slidesToScroll: 1,
-                        }
-                    },
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
+                ]
+            }
+        } if (slider.hasClass('services')) {
+            extraOptions = {
+                slidesToShow: 2.5,
+                slidesToScroll: 1,
+                centerMode: false,
+                arrows: true,
+                dots: false,
+                autoplay: true,
+                responsive: [{
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 992,
+                    settings: {
+                        slidesToShow: 1.5,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 576,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                    }
+                },
                 ]
             }
         }
